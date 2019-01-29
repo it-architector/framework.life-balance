58d04e00bfdb3253bde26ab40f7643df39da0f0c
Beloyar Chayka <poryadok2017@yandex.ru>
2019-01-29 19:36:24 +0300

    Корректировка на версию ядра: v1.1.8.16 / интерфейса: v1.0.7.18

--- Компоненты ядра/5.Модули/Суть модулей.php
+++ Компоненты ядра/5.Модули/Суть модулей.php
@@ -4,6 +4,6 @@
 require_once DIR_GITHUB_MODULES . 'autoload.php';
 
 /* Подключаем модуль базы данных mysql */
-require_once DIR_MODULE_DATA_BASE . '7.Наработка модуля базы данных mysql.php';
+require_once DIR_MODULE_DATA_BASE . '8.Наработка модуля базы данных mysql.php';
 
 ?>
