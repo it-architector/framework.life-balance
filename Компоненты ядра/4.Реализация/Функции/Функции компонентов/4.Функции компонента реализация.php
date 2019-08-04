2528957c3c2c7b68ce883a4882c39cc938462dee
Beloyar Chayka <poryadok2017@yandex.ru>
2019-08-04 17:51:16 +0300

    Процесс ядра в реализации

--- Компоненты ядра/4.Реализация/Функции/Функции компонентов/4.Функции компонента реализация.php
+++ Компоненты ядра/4.Реализация/Функции/Функции компонентов/4.Функции компонента реализация.php
@@ -8,6 +8,54 @@ class Realization
 
     static function initiation($parameters){
 
+        /* Включаем контроль ядра */
+        Conditions::initiation([]);
+
+        /* Определяем вывод информации */
+        Space::initiation([]);
+
+        /* Подготавливаем работу с ресурсами */
+        Distribution::initiation([]);
+
+        /* Разбираем запрос */
+        Conditions::parse_request([]);
+
+        /* Разбираем авторизованность */
+        Conditions::parse_authorized([]);
+
+        /* Проверяем запрос на правомерность */
+        Conditions::check_request_legality([]);
+
+        /* Создаем комуникацию с памятью */
+        Distribution::create_communication_with_memory([]);
+
+        /* Проверяем изменения в схеме базы данных */
+        Conditions::check_changes_schema_data_base([]);
+
+        /* Создаем комуникацию с базой данных */
+        Distribution::create_communication_with_data_base([]);
+
+        /* Проверяем запрос на доступность */
+        Conditions::check_request_access([]);
+
+        /* Разбираем параметры запроса */
+        Conditions::parse_parameters_request([]);
+
+        /* Проверяем запрос на деструктив */
+        Conditions::check_request_destructive([]);
+
+        /* Выполняем запрошенную наработанную цель */
+        Realization::execute_request_experience_goal([]);
+
+        /* Проверяем ответ на правильность */
+        Conditions::check_answer_correct([]);
+
+        /* Результат выполнения в интерфейс */
+        Realization::result_executed_to_interface([]);
+
+        /* Прекращаем работу ядра */
+        Conditions::stop_core([]);
+
     }
 
     static function execute_request_experience_goal($parameters){
--- Ядро.php
+++ Ядро.php
@@ -53,60 +53,8 @@ use \Framework_life_balance\core_components\Conditions;
 use \Framework_life_balance\core_components\Distribution;
 use \Framework_life_balance\core_components\Realization;
 
-/* Включаем контроль ядра */
-Conditions::initiation([]);
+/* Запускаем процесс ядра */
+\Framework_life_balance\core_components\Realization::initiation([]);
 
-/* Определяем вывод информации */
-Space::initiation([]);
-
-/* Подготавливаем работу с ресурсами */
-Distribution::initiation([]);
-
-/* Подготавливаем работу движений */
-Realization::initiation([]);
-
-/*
- * 3 этап:
- * Процесс.
- */
-
-/* Разбираем запрос */
-Conditions::parse_request([]);
-
-/* Разбираем авторизованность */
-Conditions::parse_authorized([]);
-
-/* Проверяем запрос на правомерность */
-Conditions::check_request_legality([]);
-
-/* Создаем комуникацию с памятью */
-Distribution::create_communication_with_memory([]);
-
-/* Проверяем изменения в схеме базы данных */
-Conditions::check_changes_schema_data_base([]);
-
-/* Создаем комуникацию с базой данных */
-Distribution::create_communication_with_data_base([]);
-
-/* Проверяем запрос на доступность */
-Conditions::check_request_access([]);
-
-/* Разбираем параметры запроса */
-Conditions::parse_parameters_request([]);
-
-/* Проверяем запрос на деструктив */
-Conditions::check_request_destructive([]);
-
-/* Выполняем запрошенную наработанную цель */
-Realization::execute_request_experience_goal([]);
-
-/* Проверяем ответ на правильность */
-Conditions::check_answer_correct([]);
-
-/* Результат выполнения в интерфейс */
-Realization::result_executed_to_interface([]);
-
-/* Прекращаем работу ядра */
-Conditions::stop_core([]);
 
 ?>
